0de0fa0b3d6a026417a86a0cc99e5c79eef14067
Diogo Santana <diogosantanafreitasuna@gmail.com>
2023-10-02 10:33:58 -0300

    feat: opções para os tipos de ocorrência

--- frontend/components/Select.tsx
+++ frontend/components/Select.tsx
@@ -7,12 +7,36 @@ type Select = {
     handleChange:  (event: React.ChangeEvent<HTMLSelectElement>) => void;
 }
 
+const policeOccurrences = [
+    "Roubo à mão armada",
+    "Assalto a residência",
+    "Assalto a estabelecimento comercial",
+    "Furto de veículo",
+    "Homicídio",
+    "Tráfico de drogas",
+    "Violência doméstica",
+    "Agressão física",
+    "Sequestro",
+    "Roubo de identidade",
+    "Fraude financeira",
+    "Estupro",
+    "Tentativa de homicídio",
+    "Ameaça",
+    "Incêndio criminoso",
+    "Roubo de carga",
+  ];
+  
 export function Select ({id, value, label, handleChange}: Select) {
     return (
         <div>
             <label htmlFor={id}>{label}</label>
             <select id={id} value={value} onChange={handleChange} >
                 <option value="">Selecione uma opção</option>
+                {
+                    policeOccurrences.map(occurrence => (
+                        <option value={occurrence} key={occurrence}>{occurrence}</option>
+                    ))
+                }
             </select>
         </div>
     )
