bf6d667ac5fe4073e48ecc00f05ea2f952a8af5a
Diogo Santana <diogosantanafreitasuna@gmail.com>
2023-10-02 10:31:39 -0300

    feat: adicionar nova ocorrência

--- frontend/src/pages/CreateOccurrence.tsx
+++ frontend/src/pages/CreateOccurrence.tsx
@@ -1,17 +1,27 @@
+import React from 'react';
 import {Input} from '../../components/Input'
 import {Select} from '../../components/Select'
 import {useState} from 'react'
+import {useOccurrence} from '../hooks/useOccurrences';
 
 export function CreateOccurrence () {
 
     const [title, setTitle] = useState('')
-    const [dateTime, setDateTime] = useState('');
+    const [dateTime, setDateTime] = useState(Date);
     const [type, setType] = useState('');
+
+    const {registryOccurrence} = useOccurrence();
+
+    function submitForm ( ev: React.FormEvent<HTMLFormElement> ) {
+        ev.preventDefault();
+        const id =  Math.random() * 5000
+        registryOccurrence({id, title, type, dateTime});   
+    }
 
     return (
         <div>
             <h1>Adicionar ocorrência</h1>
-            <form>
+            <form onSubmit={submitForm}>
                 <Input id='title' label='Título:' type='text' value={title} handleChange={(ev) => setTitle(ev.target.value)} />
                 <Input id='date-time' label='Data e hora:' type='datetime-local' value={dateTime} handleChange={(ev) => setDateTime(ev.target.value)} />
                 <Select id='type' label='Tipo:' value={type} handleChange={(ev) => setType(ev.currentTarget.value)}/>
