ba7ff769216da6147379574c4f278e6eb95b7900
Diogo Santana <diogosantanafreitasuna@gmail.com>
2023-10-05 23:19:33 -0300

    style: estilizando área de texto

--- frontend/src/components/Textarea.tsx
+++ frontend/src/components/Textarea.tsx
@@ -11,7 +11,7 @@ export function Textarea ({name, id, label, value, handleChange}: TextareaProps)
     return (
         <div className="flex flex-col gap-1">
             <label htmlFor={id}>{label}</label>
-            <textarea name={name} id={id} value={value} onChange={handleChange} className='border border-black rounded h-8 h-24 p-2 ml-1 w-96'></textarea>
+            <textarea name={name} id={id} value={value} onChange={handleChange} className='border border-black rounded h-24 p-2 ml-1 w-96'></textarea>
         </div>
     )
 }
