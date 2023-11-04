50b1bd57d87f449662012361eb5530160bea1e18
Diogo Santana <diogosantanafreitasuna@gmail.com>
2023-11-03 21:14:57 -0300

    fix: renomeando ocorrências p/ dashboard no navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -7,7 +7,7 @@ export function Navbar () {
     return (
         <div>
         <nav className="flex gap-10">
-            <Link to='/' className={`${pathname === '/'? 'border-b-2 border-blue-600': ''}`}>Ocorrências</Link>
+            <Link to='/' className={`${pathname === '/'? 'border-b-2 border-blue-600': ''}`}>Dashboard</Link>
              <Link to='mapa' className={`${pathname === '/mapa'? 'border-b-2 border-blue-600': ''}`}>Mapa</Link>
         </nav>
      </div>
