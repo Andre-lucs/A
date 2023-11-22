2e6a475158a33d95c344f4b007b58e1f94e54c53
Andre-lucs <andreltd312@gmail.com>
2023-11-22 16:39:58 -0300

    base test env

---
+++ Backend/src/controllers/tests/OcorrenciaController.test.js
@@ -0,0 +1,9 @@
+import OcorrenciaController from '../OcorrenciaController';
+
+describe('OcorrenciaController', () => {
+
+  test("ta funcionando?", () => {
+    expect(true).toBe(true);
+  })
+
+});
