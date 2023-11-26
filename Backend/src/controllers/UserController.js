9ae9b211ccd362c8db6ba966be762079254963be
Diogo Santana <diogosantanafreitasuna@gmail.com>
2023-11-26 13:51:01 -0300

    feat: adicionando controllers de usuário

--- Backend/src/controllers/UserController.js
+++ Backend/src/controllers/UserController.js
@@ -1,26 +1,28 @@
-import { User } from "../models/User";
-import {compare, genSalt, hash} from "bcryptjs"
-import jwt, { sign } from 'jsonwebtoken'
+import  {User}  from "../models/User.js";
+import bcryptjs from "bcryptjs"
+import jwt from 'jsonwebtoken'
 import 'dotenv/config'
 
-export const createUser = async (req, res) => {
+
+export const register = async (req, res) => {
     const {email, name, password} = req.body;
 
     try{
         if(!email || !name || !password)
             return res.status(400).send('Insira todas as informações!')
-        const userExists = User.findById({email});
+        const userExists = await User.findOne({email});
         if(!userExists) {
-            const salt = genSalt(10);
-            const pass = await hash(password, salt);
+            const salt = await bcryptjs.genSalt(10);
+            const pass = await bcryptjs.hash(password, salt);
             const newUser = new User({email, name, password: pass})
             await newUser.save();
-            const token = await jwt.sign({id: newUser._id}, process.env.SECRET_KEY, {expiresIn: process.env.JWT_EXPIRE})
-            return res.cookie({ 'token': token }).json({ success: true, message: 'Usuário cadastrado com sucesso!', data: newUser})
+            const token = jwt.sign({id: newUser._id}, process.env.SECRET_KEY, {expiresIn: process.env.JWT_EXPIRES})
+            console.log(token)
+            return res.cookie('token', token).json({ success: true, message: 'Usuário cadastrado com sucesso!', data: newUser})
         }
-        return res.status(201).send('Já existe um usuário com esse email');
+        return res.status(400).send('Já existe um usuário com esse email');
     } catch (err) {
-        res.status(404).json({err})
+        res.status(500).json({err})
     }
 }
 
@@ -35,13 +37,14 @@ export const login = async (req, res) => {
         if(!userExists)
             return res.status(404).json({message: 'Email ou senha inválidos'});
 
-        const isPasswordMatched = await compare(password, userExists.password);
+        const isPasswordMatched = await bcryptjs.compare(password, userExists.password);
         if(!isPasswordMatched)
             return res.status(404).json({message: "Email ou senha inválidos"});
 
-        const token = sign({id: userExists._id, name: userExists.name, email: userExists.email}, process.env.SECRET_KEY, {expiresIn: process.env.JWT_EXPIRE});
-        return res.status(200).cookie({"token": token}).json({message: "Usuário logado com sucesso!", isAuthenticated: true});
+        const token = jwt.sign({id: userExists._id}, process.env.SECRET_KEY, {expiresIn: process.env.JWT_EXPIRES});
+        console.log(token)
+        return res.status(200).cookie("token", token).json({message: "Usuário logado com sucesso!", isAuthenticated: true});
     } catch (err) {
-
+        res.status(500).send({err})
     }
 }
