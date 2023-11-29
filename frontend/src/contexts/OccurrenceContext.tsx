052984f92cf578cc95fbb4fda1b243da95160fc1
Diogo Santana <diogosantanafreitasuna@gmail.com>
2023-11-29 13:32:08 -0300

    fix: renomeando caminhos das rotas

--- frontend/src/contexts/OccurrenceContext.tsx
+++ frontend/src/contexts/OccurrenceContext.tsx
@@ -50,7 +50,7 @@ export function OccurrenceProvider ({children}: IOccurrenceProviderProps) {
     useEffect(() => {
         const fetchOccurrences = async () => {
             try {
-                const response = await fetch('http://localhost:3000/');
+                const response = await fetch('http://localhost:3000/ocorrencia');
                 const occurrencesRes: OccurrenceResApi[] = await response.json();
                 const data = occurrencesRes.map(({_id, title, type, date, description, location}) => {
                     return {_id, title, type, date, description, location: {lat: location.coordinates[1], lng: location.coordinates[0]} }
@@ -67,7 +67,7 @@ export function OccurrenceProvider ({children}: IOccurrenceProviderProps) {
 
     const registryOccurrence = async (occurrence: { title: string, type: string, date: string, description: string, location: {lat: number, lng:number} }): Promise<void> => {
         try {
-            const response = await fetch('http://localhost:3000', {
+            const response = await fetch('http://localhost:3000/ocorrencia', {
                 method: 'POST',
                 headers: {
                     'Content-type': 'application/json'
@@ -90,7 +90,7 @@ export function OccurrenceProvider ({children}: IOccurrenceProviderProps) {
     const deleteOccurrence = async (idOccurren: string) => {
         const occurrenceExists = findOccurrence(idOccurren);
         try {
-            await fetch(`http://localhost:3000/${idOccurren}`, {
+            await fetch(`http://localhost:3000/ocorrencia/${idOccurren}`, {
                 method: 'DELETE'
             })
             if(occurrenceExists) {
